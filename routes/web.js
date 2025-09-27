50243eee627577f31dd554be800b897d6377a9af
Dherenn <dheren90@gmail.com>
2025-09-27 18:41:13 +0700

    27 Sept

--- routes/web.js
+++ routes/web.js
@@ -1,17 +1,12 @@
 import express from "express"
+import * as profileController from "../controller/profileController.js"
 
 const web = express.Router()
 
 web.get('/', (req, res) => {
     res.render ('index')
 })
-web.get('/:username', (req, res) =>{
-    const username = req.params.username
-    res.render( "public-profile" , {
-        title : username,
-        username : username,
-        bio: "Ini adalah Aku! Raja Mexico! El Matadore Salvador Tequila El Conthole"
-    })    
-})
+web.get('/:username', profileController.publicProfile)
+
 
 export default web
