b4f85bf0deef1b05f5b990535db29109ea98b221
Dherenn <dheren90@gmail.com>
2025-10-04 18:42:28 +0700

    4 Okt

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,19 @@
+import UserModel from "../models/userModel.js";
+
 export const register = async(req, res) => {
-    try {res.status(201).json({
+    try {
+        const registerData = req.body
+
+
+        console.log(registerData);
+
+        await UserModel.create({
+            username : registerData.username,
+            email : registerData.email,
+            password : registerData.password
+        })
+
+        res.status(201).json({
             message : "Berhasil Register, Silahkan Login Kembali",
             data : null
         })
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 import express from "express"
 import * as authController from "../controller/authController.js"
-const app = express.Router()
+const api = express.Router()
 
 api.post('/register', authController.register)
 
