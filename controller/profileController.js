a9cb6ab64548118998801093c696b2d4d6889315
Dherenn <dheren90@gmail.com>
2025-10-22 20:40:43 +0700

    22 oktober

--- controller/profileController.js
+++ controller/profileController.js
@@ -3,7 +3,23 @@ export const publicProfile = (req, res) =>{
     res.render( "public-profile" , {
         title : username,
         username : username,
-        bio: "Dunia Pasti Ada Akhirnya"
+        bio: "Dunia Pasti Ada Akhirnya, Bintang-Bintang Pun Ada Umurnya"
     })
 
 }
+
+export const privateProfile = async(req, res)=>{
+    try{ 
+        res.status(200).json({
+            message: "Private Profile Berhasil Di Akses",
+            data:null,
+        });
+
+    }catch (error) {
+        res.status(500).json({
+            message: error.message,
+            data:null,
+        });
+    }
+
+};
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,14 @@
-import express from "express"
-import * as authController from "../controller/authController.js"
-const api = express.Router()
+import express from "express";
+import * as authController from "../controller/authController.js";
+import * as profileController from "../controller/profileController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
-api.post('/register', authController.register)
-api.post('/login', authController.login)
-export default api 
+const api = express.Router();
+
+api.post('/register', authController.register);
+api.post('/login', authController.login);
+
+api.get('/me',protect ,profileController.privateProfile);
+
+export default api; 
 
